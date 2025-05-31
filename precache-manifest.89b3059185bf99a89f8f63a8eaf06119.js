abc979119a6481eca53a1dc57fe4a0936a0fea09
Shuubhii <shubhijain128@gmail.com>
2025-05-31 17:43:51 +0530

    Manual deploy

---
+++ precache-manifest.89b3059185bf99a89f8f63a8eaf06119.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e510817f77a6db1d03a5b34cff088b35",
+    "url": "/FinalFiles/index.html"
+  },
+  {
+    "revision": "085025dd49c22753234f",
+    "url": "/FinalFiles/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6ddef300c0266706a401",
+    "url": "/FinalFiles/static/js/2.bed62cdb.chunk.js"
+  },
+  {
+    "revision": "e93062fc5108de86cfe6f6cfea2f9e1a",
+    "url": "/FinalFiles/static/js/2.bed62cdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3726123e9199d9d0b042",
+    "url": "/FinalFiles/static/js/3.8df07351.chunk.js"
+  },
+  {
+    "revision": "085025dd49c22753234f",
+    "url": "/FinalFiles/static/js/main.dcd0e193.chunk.js"
+  },
+  {
+    "revision": "525dd906b9430617365a",
+    "url": "/FinalFiles/static/js/runtime-main.39b04bd2.js"
+  }
+]);
